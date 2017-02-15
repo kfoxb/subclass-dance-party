4e98fae723899a31af2a15301fad66114c0c57ca
kfoxb <kfoxb@kfoxb.com>
2017-02-14 16:47:10 -0800

    Pass all minimum requirements

--- src/init.js
+++ src/init.js
@@ -73,27 +73,94 @@ $(document).ready(function() {
 
     // debugger;
     var batman = new MakeBatmanDancer(top, left, 400);
-    console.log(batman.$node[0]);
+    //console.log(batman.$node[0]);
     window.dancers.push(batman);
     $('body').append(batman.$node);
   });
 
 
-  $('body').on('mouseenter', '.sidetoside', function() {
-    $(this).hide();
-    console.log(this);
+  // $('body').on('mouseenter', '.batman', function() {
+  //   $(this).hide();
+  //   //console.log(this);
+  //   var batman = $(this)[0];
+
+  //   var topString = batman.style.top;
+  //   var top = Number(topString.substring(0, topString.length - 2));
+
+  //   var leftString = batman.style.left;
+  //   var left = Number(leftString.substring(0, leftString.length - 2));
+
+  //   var joker = new MakeJokerDancer(top, left, 400);
+  //   //console.log(joker.$node[0]);
+  //   window.dancers.push(joker);
+  //   $('body').append(joker.$node);
+  // });
+
+  $('body').on('mouseenter', '.batman', function() {
+    //debugger;
+    var min = Infinity;
     var batman = $(this)[0];
+    var batmanTopString = batman.style.top;
+    var batmanTop = Number(batmanTopString.substring(0, batmanTopString.length - 2));
 
-    var topString = batman.style.top;
-    var top = Number(topString.substring(0, topString.length - 2));
+    var batmanLeftString = batman.style.left;
+    var batmanLeft = Number(batmanLeftString.substring(0, batmanLeftString.length - 2));
+    var closet = null;
 
-    var leftString = batman.style.left;
-    var left = Number(leftString.substring(0, leftString.length - 2));
+ 
 
-    var joker = new MakeJokerDancer(top, left, 400);
-    //console.log(joker.$node[0]);
-    window.dancers.push(joker);
-    $('body').append(joker.$node);
+    for (var i = 0; i < window.dancers.length; i++) {
+      if (window.dancers[i].constructor === MakeBatmanDancer) {
+        continue;
+      }
+      var topString = window.dancers[i].$node[0].style.top;
+      var top = Number(topString.substring(0, topString.length - 2));
+      var leftString = window.dancers[i].$node[0].style.left;
+      var left = Number(leftString.substring(0, leftString.length - 2));
+      console.log(top + left);
+      var val = (top - batmanTop) * (top - batmanTop) + (left - batmanLeft) * (left - batmanLeft);
+      if (val < min) {
+        min = val;
+        closet = window.dancers[i];
+      }
+    }
+    //debugger;
+    if (closet !== null) {
+      var jokerTop = Number(closet.$node[0].style.top.substring(0, closet.$node[0].style.top.length - 2));
+      var jokerLeft = Number(closet.$node[0].style.left.substring(0, closet.$node[0].style.left.length - 2));
+
+      // $(this).addClass(move);
+      var leftDiff = jokerLeft - batmanLeft;
+      var topDiff = jokerTop - batmanTop;
+     
+      var move = {
+        'transform': 'translate(' + leftDiff + 'px,' + topDiff + 'px)',
+        'transition-duration': '4s'
+      };
+      $(this).css(move);
+      // make joker disappear
+      setTimeout(closet.$node.hide.bind(closet.$node), 2000);
+      // remove joker from dancers array
+      for (var i = 0; i < window.dancers.length; i++) {
+        if (window.dancers[i] === closet) {
+          window.dancers.splice(i, 1);
+        }
+      }
+
+      var styleSettings = {
+        top: jokerTop,
+        left: jokerLeft,
+        position: 'absolute',
+        height: '160px',
+        width: '160px',
+        display: 'none'
+      };
+      var $kapow = $('<img src="img/kapow.png">');
+      $kapow.css(styleSettings);
+      $('body').append($kapow);
+      setTimeout($kapow.show.bind($kapow), 2000);
+      setTimeout($kapow.hide.bind($kapow), 3000);
+    }
   });
 });
 
